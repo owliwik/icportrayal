a8d362985fca55a7a5c3482194e4d5f22cac3401
Owliwik Li <michael@owliwik.com>
2024-10-22 14:22:22 +0800

    Remove auth api

--- app/api/auth/session/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import * as sdk from 'node-appwrite'
-import { getSession } from '@/app/api/appwrite'
-import { cookies } from 'next/headers'
-
-export const GET = async (req: Request) => {
-  const { status, session } = await getSession()
-  return NextResponse.json(session, { status })
-}
