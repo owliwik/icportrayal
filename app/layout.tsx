df763e790f3b711ca23b42bcdc4f4b71159401b4
Owliwik Li <michael@owliwik.com>
2024-10-22 14:02:44 +0800

    First publish

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import '@/styles/globals.css'
 import { Navigation } from '@/components/navbar'
-import { ProfileProvider } from '@/components/ProfileContext'
 import { LanguageProvider } from '@/components/LanguageContext'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -10,12 +9,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang='en'>
       <body className='w-full min-h-[100vh] h-1 flex flex-col'>
         <LanguageProvider>
-          <ProfileProvider>
-            <Navigation />
-            <main className='flex-1 bg-[#F7F9FE] bg-opacity-80'>
-              {children}
-            </main>
-          </ProfileProvider>
+          <Navigation />
+          <main className='flex-1 bg-[#F7F9FE] bg-opacity-80'>{children}</main>
         </LanguageProvider>
       </body>
     </html>
